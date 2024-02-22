e504b789127585f944b8fe503b42463a09982b9c
Ana João Alves <a95128@alunos.uminho.pt>
2024-02-22 21:25:15 +0000

    Add files via upload

--- TP2/README.md
+++ TP2/README.md
@@ -1,1 +1,27 @@
 # PL2024
+
+## Autor 
+
+**Nome:** Ana Joao Alves
+
+**ID:** A95128
+
+# Explicação do Código
+
+Este é um breve resumo do que o código Python fornecido faz. O código é projetado para converter texto escrito em Markdown para HTML. Vamos analisar cada parte do código:
+
+## Função `markdown_to_html(markdown_text)`
+
+Esta função recebe uma string contendo texto em formato Markdown como entrada e utiliza expressões regulares para encontrar padrões específicos no texto Markdown. Em seguida, substitui esses padrões pelos equivalentes em HTML. 
+- Títulos Markdown são convertidos para tags HTML de cabeçalho (`<h1>`, `<h2>`, etc.).
+- Negrito e itálico são convertidos para tags HTML `<b>` e `<i>`, respectivamente.
+- Listas ordenadas e não ordenadas são convertidas para tags HTML `<ol>` e `<ul>`, com cada item da lista convertido para uma tag `<li>`.
+- Imagens e links Markdown são convertidos para as tags HTML `<img>` e `<a>`, respectivamente.
+- Linha horizontal (`---`) é convertida para a tag HTML `<hr>`.
+- Citação em bloco (`> texto`) é convertida para a tag HTML `<blockquote>`.
+- Blocos de código (delimitados por ``` ... ```) são convertidos para a tag HTML `<code>`.
+
+## Função `main()`
+
+Esta função é a função principal do programa. Ela lê o texto Markdown da entrada padrão, chama a função `markdown_to_html()` para converter o texto Markdown em HTML e escreve o HTML resultante em um arquivo chamado `'output.html'`.
+
--- TP2/tpc2.py
+++ TP2/tpc2.py
@@ -2,6 +2,10 @@ import sys
 import re
 
 def markdown_to_html(markdown_text):
+
+     # Citação em bloco
+    markdown_text = re.sub(r'^>\s(.*?)$', r'<blockquote>\1</blockquote>', markdown_text, flags=re.MULTILINE)
+
     # Cabeçalhos
     markdown_text = re.sub(r'^#{1}\s+(.+?)\s*$', r'<h1>\1</h1>', markdown_text, flags=re.MULTILINE)
     markdown_text = re.sub(r'^#{2}\s+(.+?)\s*$', r'<h2>\1</h2>', markdown_text, flags=re.MULTILINE)
@@ -10,17 +14,26 @@ def markdown_to_html(markdown_text):
     markdown_text = re.sub(r'^#{5}\s+(.+?)\s*$', r'<h5>\1</h5>', markdown_text, flags=re.MULTILINE)
     markdown_text = re.sub(r'^#{6}\s+(.+?)\s*$', r'<h6>\1</h6>', markdown_text, flags=re.MULTILINE)
 
-    # Citação em bloco
-    markdown_text = re.sub(r'^>\s(.*?)$', r'<blockquote>\1</blockquote>', markdown_text, flags=re.MULTILINE)
+    # Negrito
+    markdown_text = re.sub(r'\*{2}(.*?)\*{2}', r'<b>\1</b>', markdown_text)
+
+    # Itálico
+    markdown_text = re.sub(r'\*{1}(.*?)\*{1}', r'<i>\1</i>', markdown_text)
+
+    # Linha horizontal
+    markdown_text = re.sub(r'\-{3}', r'<hr>', markdown_text)
+
+    # Código
+    markdown_text = re.sub(r'```(.*?)```', r'<code>\1</code>', markdown_text)
+
+    # Lista ordenada
+    markdown_text = re.sub(r'(\d+\.\s+.*\n?)+', r'<ol>\g<0></ol>', markdown_text)
+    markdown_text = re.sub(r'\d+\.\s+(.*)', r'<li>\1</li>\n', markdown_text)
 
     # Lista não ordenada
     markdown_text = re.sub(r'^-\s+(.+)', r'<ul>\n<li>\1</li>', markdown_text, flags=re.MULTILINE)
     markdown_text = re.sub(r'</li>\n<ul>', r'</li>\n</ul>\n<ul>', markdown_text)
     markdown_text += '</ul>'
-
-    # Lista ordenada
-    markdown_text = re.sub(r'(\d+\.\s+.*\n?)+', r'<ol>\g<0></ol>', markdown_text)
-    markdown_text = re.sub(r'\d+\.\s+(.*)', r'<li>\1</li>\n', markdown_text)
 
     # Imagem
     markdown_text = re.sub(r'\!{1}\[{1}(.*?)\]{1}\({1}(.*?)\){1}', r'<img src="\2" alt="\1"/>', markdown_text)
@@ -28,17 +41,8 @@ def markdown_to_html(markdown_text):
     # Link
     markdown_text = re.sub(r'\[{1}(.*?)\]{1}\({1}(.*?)\){1}', r'<a href="\2">\1</a>', markdown_text)
 
-    # Linha horizontal
-    markdown_text = re.sub(r'\-{3}', r'<hr>', markdown_text)
 
-    # Código
-    markdown_text = re.sub(r'```(.+?)```', r'<code>\1</code>', markdown_text)
 
-    # Negrito
-    markdown_text = re.sub(r'\*{2}(.*?)\*{2}', r'<b>\1</b>', markdown_text)
-
-    # Itálico
-    markdown_text = re.sub(r'\*{1}(.*?)\*{1}', r'<i>\1</i>', markdown_text)
 
     return markdown_text
 
